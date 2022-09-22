44a92722ec55337950d1c86209fa1df47c3c80fc
jayesh <jayeshborse0705@gmail.com>
2022-09-21 22:30:57 -0700

    [jayesh]search  phone Number Regex Pattern

--- UserRegistrationRegex/UserRegistrationRegex/Pattern.cs
+++ UserRegistrationRegex/UserRegistrationRegex/Pattern.cs
@@ -10,6 +10,8 @@ namespace UserRegistrationRegex
     public class Pattern
     {
         Regex regex = new Regex("^[A-Z]{1}[A-Za-z]{2,}$");
+        Regex regex1 = new Regex("^[A-Za-z0-9+_.-]+@[a-zA-Z0-9.-]+$");
+        Regex regex2 = new Regex("^[0-9]{2}[_][0-9]{10}$");
 
 
         public void ValidFirstName(string First_Name)
@@ -36,6 +38,29 @@ namespace UserRegistrationRegex
                 Console.WriteLine("Invalid");
             }
         }
-
+        public void ValidEmail_Id(string Email_id)
+        {
+            Console.WriteLine("Enter Email.id:" + Email_id);
+            if (regex1.IsMatch(Email_id))
+            {
+                Console.WriteLine("valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+        public void ValidPhoneNO(string Phone_No)
+        {
+            Console.WriteLine("Enter phoneNo:" + Phone_No);
+            if (regex2.IsMatch(Phone_No))
+            {
+                Console.WriteLine("valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
     }
 }
